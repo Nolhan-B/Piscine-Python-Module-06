0e70e059c4576ae59217eba04275d01a823c9ba3
nbilyj <nbilyj@k2r3p9.42mulhouse.fr>
2026-02-03 17:16:46 +0100

    wip part 3

--- alchemy/potions.py
+++ alchemy/potions.py
@@ -1,22 +1,22 @@
 import alchemy.elements as e
 
 
-def healing_potion() -> None:
+def healing_potion() -> str:
     return (f"Healing potion brewed with {e.create_fire()} "
             f"and {e.create_water()}")
 
 
-def strength_potion() -> None:
+def strength_potion() -> str:
     return (f"Strength potion brewed with {e.create_earth()} "
             f"and {e.create_fire()}")
 
 
-def invisibility_potion() -> None:
+def invisibility_potion() -> str:
     return (f"Invisibility potion brewed with {e.create_air()} "
             f"and {e.create_water()}")
 
 
-def wisdom_potion() -> None:
+def wisdom_potion() -> str:
     return ("Wisdom potion brewed with all elements"
             f"{e.create_air()}, {e.create_earth()}, "
             f"{e.create_fire()}, {e.create_water()}")
